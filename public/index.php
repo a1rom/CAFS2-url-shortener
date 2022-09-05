fc23c1f3d326b3884a390c5a416e25443d2f094a
alrom12 <dev@alrom.de>
2022-09-05 18:44:26 +0300

    json files

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 session_start();
 ob_start();
 require_once '../config/config.php';
-require_once ConfigRoutes::APP . 'helpers.php';
-require_once ConfigRoutes::CLASSES . 'UserSubmitCache.php';
-$submitCache = new UserSubmitCache();
 try {
+    require_once ConfigRoutes::APP . 'helpers.php';
+    require_once ConfigRoutes::CLASSES . 'UserSubmitCache.php';
+    $submitCache = new UserSubmitCache();
     require ConfigRoutes::ROUTES . 'router.php';
     ob_end_flush();
 } catch (Exception $e) {
